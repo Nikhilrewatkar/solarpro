2985967eda4bb8bb5a39336ff06dc134aec95e34
Nikhil <Nikhilrewatkar1993@gmail.com>
2025-06-09 21:28:06 +0530

    update topnavbar

--- src/app/components/navigation/topnav/topnav.component.ts
+++ src/app/components/navigation/topnav/topnav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topnav',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopnavComponent implements OnInit {
 
+  isDropdownOpen = false;
+  activeDropdown: string | null = null;
+  navlinks = [
+    {
+      'id': 1, 'title': 'our offerings', 'caret': true, 'children': [
+        { 'subtitle': 'offering 1' },
+        { 'subtitle': 'offering 2' },
+        { 'subtitle': 'offering 3' },
+      ]
+    },
+    {
+      'id': 2, 'title': 'solar solutions', 'caret': true, 'children': [
+        { 'subtitle': 'solutions 1' },
+        { 'subtitle': 'solutions 2' },
+        { 'subtitle': 'solutions 3' },
+      ]
+    },
+    { 'id': 3, 'title': 'our presence' },
+    { 'id': 4, 'title': 'blog' },
+    { 'id': 5, 'title': 'more' },
+  ]
+
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
 
+  toggleDropdown(menu: string, event: MouseEvent): void {
+    event.stopPropagation(); // Prevent outside click from closing immediately
+    this.activeDropdown = this.activeDropdown === menu ? null : menu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.menu-item')) {
+      this.activeDropdown = null;
+    }
+  }
+
 }
+
+
+
+
+
