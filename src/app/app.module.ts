d4494006def55950d64d5926e6a4254ed84c4ef5
Nikhil <Nikhilrewatkar1993@gmail.com>
2025-06-09 21:15:45 +0530

    update components, services and labiraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './components/navigation/topnav/topnav.component';
+import { FooterComponent } from './components/navigation/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    FooterComponent,
+    HomeComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
